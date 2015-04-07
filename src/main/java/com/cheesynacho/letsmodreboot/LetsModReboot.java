9b33effdd8314fa304050672742d4a5ed33e027f
Mitchell Rath <mitchell.rath.3@hotmail.com>
2015-04-06 18:32:00 -0700

    Setup
    Added FML files.
    Setup IDEA workspace.
    Created Mod Class.

---
+++ src/main/java/com/cheesynacho/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,32 @@
+package com.cheesynacho.letsmodreboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="LetsModReboot", name="Let's Mod Reboot", version="1.7.2-1.0")
+public class LetsModReboot
+{
+    @Mod.Instance("LetsModReboot")
+    public static LetsModReboot instance;
+
+    @Mod.EventHandler
+    public void preInt(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
